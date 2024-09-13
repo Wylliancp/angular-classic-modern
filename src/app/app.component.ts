50b1fb25437948107b4e9469f1725f2398abf53d
wyllian costa <wylliancp@gmail.com>
2024-09-13 13:35:57 -0300

    O commit ira adicionar o angular classic (Module e RxJs)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styles: [],
+  template: `
+  <app-header></app-header>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'angular-classic-modern';
